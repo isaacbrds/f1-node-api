e4c416ab1368e31a688583ea17cc1a4f1b8bd1af
isaac <isaac.brigido@gmail.com>
2025-07-06 21:40:34 -0300

    feat: implemtando get Team

--- src/server.ts
+++ src/server.ts
@@ -5,16 +5,33 @@ const server  = fastify({logger: true});
 server.register(cors, {
   origin: "*"
 })
+const teams = [
+  {id: 1, name: "Maclaren"},
+  {id: 2, name: "Ferrari"},
+]
+
 const drivers = [
   {id: 1, name: "Louis Hamilton"},
   {id: 2, name: "Michael Shumaker"},
 ]
 server.get("/teams", async (req, res)=>{
   res.type("application/json").code(200)
-  return[{
-    id: 1,
-    name: "Ferrari"
-  }]
+  return {teams}
+})
+interface TeamParam {
+  id: string;
+}
+
+server.get<{Params: TeamParam}>("/teams/:id",async(req, res)=>{
+  const id = parseInt(req.params.id)
+  const team = teams.find(team => team.id === id)
+   if(team){
+    res.type("application/json").code(200)
+    return {team}
+   }else{
+    res.type("application/json").code(404)
+    return {message: "Team not found"}
+   }
 })
 
 server.get("/drivers", async(req, res)=>{
